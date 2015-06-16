2a51ab3a969bd00ad8f64eae331f79d4821d9dd8
4r3 <4r3gn4ult@gmail.com>
2015-06-16 17:25:51 +0200

    amelioration prise en main editeur

--- src/Controlleur/GestionSourisInsertionEditeur.java
+++ src/Controlleur/GestionSourisInsertionEditeur.java
@@ -1,12 +1,15 @@
 package Controlleur;
 
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import Modele.Editeur;
 import Modele.Variables;
 import Vue.Panel.AirePlateauEditeur;
 
-public class GestionSourisInsertionEditeur implements MouseListener {
+public class GestionSourisInsertionEditeur implements MouseListener,
+		MouseMotionListener {
 
 	private Editeur editeur;
 
@@ -14,6 +17,7 @@ public class GestionSourisInsertionEditeur implements MouseListener {
 			Editeur editeur) {
 		this.editeur = editeur;
 		panel.addMouseListener(this);
+		panel.addMouseMotionListener(this);
 	}
 
 	@Override
@@ -31,19 +35,28 @@ public class GestionSourisInsertionEditeur implements MouseListener {
 	@Override
 	public void mouseExited(java.awt.event.MouseEvent e) {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void mousePressed(java.awt.event.MouseEvent e) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void mouseReleased(java.awt.event.MouseEvent e) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		this.editeur.modifierNiveau(e.getPoint().x / Variables.TAILLE_CASE,
+				e.getPoint().y / Variables.TAILLE_CASE);
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
 	}
 
 }
--- src/Modele/Editeur.java
+++ src/Modele/Editeur.java
@@ -2,7 +2,6 @@ package Modele;
 
 import java.util.Observable;
 
-import BoulderDash.BoulderDash;
 import Modele.Animation.ChoixAnimation;
 
 public class Editeur extends Observable {
@@ -35,34 +34,34 @@ public class Editeur extends Observable {
 	public void modifierNiveau(int x, int y) {
 		switch (iconActif) {
 		case Personnage_Idle:
-			BoulderDash.getEdit().niveau.inserePersonage(x, y);
+			niveau.inserePersonage(x, y);
 			break;
 		case Sortie:
-			BoulderDash.getEdit().niveau.insereSortie(x, y);
+			niveau.insereSortie(x, y);
 			break;
 		case Diamant:
-			BoulderDash.getEdit().niveau.insereDiamant(x, y);
+			niveau.insereDiamant(x, y);
 			break;
 		case Rocher:
-			BoulderDash.getEdit().niveau.insereRocher(x, y);
+			niveau.insereRocher(x, y);
 			break;
 		case Vide:
-			BoulderDash.getEdit().niveau.insereVide(x, y);
+			niveau.insereVide(x, y);
 			break;
 		case Boue:
-			BoulderDash.getEdit().niveau.insereBoue(x, y);
+			niveau.insereBoue(x, y);
 			break;
 		case Mur:
-			BoulderDash.getEdit().niveau.insereMurNormal(x, y);
+			niveau.insereMurNormal(x, y);
 			break;
 		case Mur_Indestructible:
-			BoulderDash.getEdit().niveau.insereMurIndestructible(x, y);
+			niveau.insereMurIndestructible(x, y);
 			break;
 		case Mur_Magique:
-			BoulderDash.getEdit().niveau.insereMurMagique(x, y);
+			niveau.insereMurMagique(x, y);
 			break;
 		case Papillon:
-			BoulderDash.getEdit().niveau.inserePapillon(x, y);
+			niveau.inserePapillon(x, y);
 			break;
 		}
 		setChanged();
